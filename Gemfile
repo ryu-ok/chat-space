37e3a6fdc4c81cf8f2323f92de924844f54077c0
ryu-ok <61011003+ryu-ok@users.noreply.github.com>
2020-02-19 22:00:08 +0900

    Revert "chat-spaceのメッセージ送信機能の実装"

--- Gemfile
+++ Gemfile
@@ -60,6 +60,3 @@ gem 'font-awesome-sass'
 gem 'devise'
 
 gem 'pry-rails'
-
-gem 'carrierwave'
-gem 'mini_magick'
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'groups#index'
   resources :users, only: [:edit, :update]
-  resources :groups, only: [:index, :new, :create, :edit, :update] do
-    resources :messages, only: [:index, :create]
-  end
+  resources :groups, only: [:index, :new, :create, :edit, :update]
 end
